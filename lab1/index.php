4f41520cd9688c600ef62805dddfb7477b43bd32
ipz_242-rdr <ipz242_rdr@student.ztu.edu.ua>
2026-02-10 20:32:40 +0200

    зроблено завдання 1 та 2

---
+++ lab1/index.php
@@ -0,0 +1,16 @@
+<?php
+echo 'Завдання 2<br>';
+echo "Полину в мріях в купель океану,<br>
+        Відчую <b>шовковистість</b> глибини,<br>
+        &nbsp;Чарівні мушлі з дна собі дістану,<br>
+        &nbsp;&nbsp;&nbsp; Щоб <i><b>взимку</b></i> <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <u>тішили</u><br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; мене<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; вони...";
+
+echo '<br>Завдання 3<br>';
+$uan = 1500;
+$usd = 43;
+$result = round($uan/$usd);
+echo "$uan грн. Можна обміняти на $result доларів<br>";
+?>
