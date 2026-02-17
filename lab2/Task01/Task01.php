01d8cb52792115e387430681394b863f0b6a1468
ipz_242-rdr <ipz242_rdr@student.ztu.edu.ua>
2026-02-17 16:09:06 +0200

    доробив завдання 01 з усіма пунктами

--- lab2/Task01/Task01.php
+++ lab2/Task01/Task01.php
@@ -1,5 +1,7 @@
 <?php
 $output = "";
+
+//1.1
 if(isset($_POST["newTextSubmit"])){
     $text = $_POST["text"] ?? "";
     $find = $_POST["find"] ?? "";
@@ -8,6 +10,7 @@ if(isset($_POST["newTextSubmit"])){
     $output = "Новий рядок: " . $result;
 }
 
+//1.2
 function sortCites($citiesStr)
 {
     $arr = explode(" ", $citiesStr);
@@ -19,6 +22,91 @@ if(isset($_POST["citiesSubmit"])){
     $citiesStr = $_POST["citiesStr"] ?? "";
     $result = sortCites($citiesStr);
     $output = "Відсортовані міста: " . $result;
+}
+
+//1.3
+function nameFile($strUrlFile)
+{
+    $arr = str_replace("\\", "/", $strUrlFile);
+    return pathinfo($arr, PATHINFO_FILENAME);
+}
+
+if(isset($_POST["nameFileSubmit"])){
+    $nameFile = $_POST["urlFile"] ?? "";
+    $result = nameFile($nameFile);
+    $output = "Назва файлу: " . $result;
+}
+
+//1.4
+function numberDays($strStartDate, $strEndDate)
+{
+    $pattern = "/^\d{2}[.-]\d{2}[.-]\d{4}$/";
+    if(!preg_match($pattern, $strStartDate) || !preg_match($pattern, $strEndDate))
+        return "Помилка дати";
+
+    $dateStart = new DateTime($strStartDate);
+    $dateEnd = new DateTime($strEndDate);
+    $interval = $dateStart->diff($dateEnd);
+    return $interval->days;
+}
+if(isset($_POST["numberDaysSubmit"])){
+    $startDate = $_POST["startDate"] ?? "";
+    $endDate = $_POST["endDate"] ?? "";
+    $result = numberDays($startDate, $endDate);
+    $output = "Кількість днів між датами: " . $result;
+}
+
+//1.5
+$result = "";
+$createPassword = $_POST["createPassword"] ?? null;
+function generatePassword($length)
+{
+    $chars = "qwertyuiopasdfghjklzxcvbnm";
+    $chars .= "0123456789";
+    $chars .= "QWERTYUIOPASDFGHJKLZXCVBNM";
+    $chars .= "!@#$%^&*()";
+
+    $password = "";
+    $maxIndex = strlen($chars) - 1;
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[random_int(0, $maxIndex)];
+    }
+    return $password;
+}
+
+
+function isStrongPassword($password)
+{
+    if (strlen($password) < 8 ||
+            !preg_match('/[A-Z]/', $password) ||
+            !preg_match('/[a-z]/', $password) ||
+            !preg_match('/[0-9]/', $password) ||
+            !preg_match('/[$@#*]/', $password) ) {
+        return false;
+    }
+
+    return true;
+}
+
+if (isset($_POST["generatePasswordSubmit"])) {
+    $passwordLength = $_POST["lengthPassword"];
+
+    $createPassword = generatePassword($passwordLength);
+
+    $output = "Згенерований пароль: " . $createPassword;
+
+}
+
+if (isset($_POST["checkPasswordSubmit"])) {
+    $passwordToCheck = $_POST["passwordCheck"];
+    $createdPassword = $passwordToCheck;
+
+    if (isStrongPassword($createPassword)) {
+        $result = "Пароль міцний";
+    } else {
+        $result = "Пароль слабкий";
+    }
 }
 ?>
 
@@ -33,5 +121,18 @@ if(isset($_POST["citiesSubmit"])){
 </head>
 <body>
 <p><?= $output?> </p>
+
+<?php if(isset($createPassword)) : ?>
+<form action="" method="post">
+    <label>Перевірити пароль:</label>
+    <input type="text" name="passwordCheck" value="<?= htmlspecialchars($createPassword) ?>"><br>
+    <input type="hidden" name="createPassword" value="<?= htmlspecialchars($createPassword) ?>">
+    <input type="submit" name="checkPasswordSubmit">
+</form>
+    <?php if ($result !== "") : ?>
+        <p><?= htmlspecialchars($result) ?></p>
+    <?php endif; ?>
+<?php endif; ?>
+<a href="index.html">Назад до завдань</a>
 </body>
 </html>
